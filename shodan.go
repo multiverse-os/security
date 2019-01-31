156d3799b41210d24fe06a646fdcd3f638abc519
Your Name <you@example.com>
2019-01-31 15:26:07 -0500

    need to grab two pages worth of data

--- shodan.go
+++ shodan.go
@@ -11,13 +11,17 @@ import (
 
 func Search(value string) (ips []string) {
 	bow := surf.NewBrowser()
-	err := bow.Open("https://www.shodan.io/search?query="+value)
-	if err != nil {
-		panic(err)
+
+	// If you are not loggedin, only 2 pages can be requested
+	for i := 1; i <= 2; i++ {
+		err := bow.Open("https://www.shodan.io/search?query="+value+"&page="+strconv.Itoa(i))
+		if err != nil {
+			fmt.Println("[Error] Failed to connect to shodan, try again shortly...")
+
+		bow.Find("div.ip").Each(func(i int, element *goquery.Selection) {
+			fmt.Print(element.Text())
+			ips = append(ips, element.Text())
+		})
 	}
-	bow.Find("div.ip").Each(func(i int, element *goquery.Selection) {
-		fmt.Print(element.Text())
-		ips = append(ips, element.Text())
-	})
 	return ips
 }
