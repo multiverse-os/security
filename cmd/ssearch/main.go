74137420f77e07289712cfd992608f804e22cc1f
Your Name <you@example.com>
2019-01-31 17:14:59 -0500

    Added login, object to hold cache and generally improved it

--- cmd/ssearch/main.go
+++ cmd/ssearch/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/lostinblue/shodan"
+	. "github.com/lostinblue/shodan"
 )
 
 func main() {
-	shodan.Search("Parity-Ethereum//v2.2.7-stable-b00a21f-20190115/x86_64-linux-gnu/rustc1.31.1")
+	shodan := New()
+	shodan.Search("Geth")
 }
--- shodan.go
+++ shodan.go
@@ -2,21 +2,52 @@ package shodan
 
 import (
 	"fmt"
+	"time"
 	"strconv"
 
 	"gopkg.in/headzoo/surf.v1"
+	browser "github.com/headzoo/surf/browser"
 	"github.com/PuerkitoBio/goquery"
 
 )
 
-func Search(value string) (ips []string, err error) {
-	bow := surf.NewBrowser()
+const DefaultPageLimit = 2
+
+type Shodan struct {
+	IPs []string
+	PageLimit int
+	Page *browser.Browser
+}
+
+func New() *Shodan {
+	return &Shodan{
+		IPs: []string{},
+		PageLimit: DefaultPageLimit,
+		Page: surf.NewBrowser(),
+	}
+}
+
+func Login(username, password string) *Shodan {
+	shodan := New()
+	err := shodan.Page.Open("https://account.shodan.io/login")
+	fm, _ := shodan.Page.Form("form.login-form")
+	fm.Input("username", username)
+	fm.Input("password", password)
+	if err != nil && fm.Submit() != nil {
+		fmt.Println("[Error] Failed to load login page, will try again shortly...")
+		time.Sleep(5)
+		Login(username, password)
+	}
+	return shodan
+}
+
+func (self *Shodan) Search(value string) (ips []string, err error) {
 	// If you are not loggedin, only 2 pages can be requested
-	for i := 1; i <= 2; i++ {
-		err = bow.Open("https://www.shodan.io/search?query="+value+"&page="+strconv.Itoa(i))
+	for i := 1; i <= self.PageLimit; i++ {
+		err = self.Page.Open("https://www.shodan.io/search?query="+value+"&page="+strconv.Itoa(i))
 		if err != nil {
 			fmt.Println("[Error] Failed to connect to shodan, try again shortly...")
-			bow.Find("div.ip").Each(func(i int, element *goquery.Selection) {
+			self.Page.Find("div.ip").Each(func(i int, element *goquery.Selection) {
 				fmt.Print(element.Text())
 				ips = append(ips, element.Text())
 			})
@@ -26,3 +57,4 @@ func Search(value string) (ips []string, err error) {
 	}
 	return ips, nil
 }
+
