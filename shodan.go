34385a35e642aa8d11c31c11b1e90d03437a6376
Your Name <you@example.com>
2019-01-31 15:31:41 -0500

    few more improvements

--- shodan.go
+++ shodan.go
@@ -9,21 +9,20 @@ import (
 
 )
 
-
-
-func Search(value string) (ips []string) {
+func Search(value string) (ips []string, err error) {
 	bow := surf.NewBrowser()
-
 	// If you are not loggedin, only 2 pages can be requested
 	for i := 1; i <= 2; i++ {
-		err := bow.Open("https://www.shodan.io/search?query="+value+"&page="+strconv.Itoa(i))
+		err = bow.Open("https://www.shodan.io/search?query="+value+"&page="+strconv.Itoa(i))
 		if err != nil {
 			fmt.Println("[Error] Failed to connect to shodan, try again shortly...")
 			bow.Find("div.ip").Each(func(i int, element *goquery.Selection) {
 				fmt.Print(element.Text())
 				ips = append(ips, element.Text())
 			})
+		}else{
+			return ips, err
 		}
 	}
-	return ips
+	return ips, nil
 }
